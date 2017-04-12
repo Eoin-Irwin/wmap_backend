f3ab8e39951b4483d18cdbbefb3341e7ae6c1dc1
Eoin-Irwin <C13439802@mydit.ie>
2017-04-12 09:25:31 +0100

    api data new timestamp instead of epoch

--- app/dublin_bikes_api.py
+++ app/dublin_bikes_api.py
@@ -1,14 +1,15 @@
 # noinspection PyCompatibility
 import requests
-import datetime
+from time import gmtime, strftime
 
 
 def json_data_import():
     r = requests.get(
         'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a6c1b6c4bac2222a2b80e49abf91bbfddd45e5e2')
     dub_json_data = r.json()
-
-    return dub_json_data
+    for i in dub_json_data:
+        i['last_update'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        return i
 
 
 json_data_import()
