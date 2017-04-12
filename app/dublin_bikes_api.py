bc7d835e7c05e6491d6e0290d0a01396e1b35d25
Eoin-Irwin <C13439802@mydit.ie>
2017-04-12 08:47:21 +0100

    get dublibikes api data

--- app/dublin_bikes_api.py
+++ app/dublin_bikes_api.py
@@ -1,9 +1,14 @@
 # noinspection PyCompatibility
-import urllib.request
-from pprint import pprint
+import requests
+import datetime
 
 
 def json_data_import():
-    x = urllib.request.urlopen(
+    r = requests.get(
         'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a6c1b6c4bac2222a2b80e49abf91bbfddd45e5e2')
-    return x.read()
+    dub_json_data = r.json()
+
+    return dub_json_data
+
+
+json_data_import()
