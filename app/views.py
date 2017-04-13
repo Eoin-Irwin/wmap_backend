b330937f5d8a873cb5a8282f88da2ba736cf5808
Eoin-Irwin <C13439802@mydit.ie>
2017-04-14 00:38:25 +0100

    View returning lat and long data to web page

--- app/views.py
+++ app/views.py
@@ -1,16 +1,14 @@
-import datetime
-
 from django.http import HttpResponse
-
+from django.template.response import TemplateResponse
 from app.dublin_bikes_api import json_data_import
-
-
-def datetime_now(request):
-    current = datetime.datetime.now()
-    html_view = "<html><body>The time is {}</body> </html>".format(current)
-    return HttpResponse(html_view)
+from .sql_selects import SELECT_ALL_LOCATIONS
 
 
 def view_json_data(request):
     json_data = json_data_import()
     return HttpResponse(json_data)
+
+
+def landing(request):
+    all_locations = SELECT_ALL_LOCATIONS
+    return TemplateResponse(request, 'app/index.html', {"data": all_locations})
