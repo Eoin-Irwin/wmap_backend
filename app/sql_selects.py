033a759cc88611113c3282f7424d4097151c1188
Eoin-Irwin <C13439802@mydit.ie>
2017-04-14 10:57:37 +0100

    all data values select

--- app/sql_selects.py
+++ app/sql_selects.py
@@ -1,7 +1,4 @@
 from app.models import DublinBikes
 
-SELECT_ALL_INFO = DublinBikes.objects.all()
-
-SELECT_ALL_LOCATIONS = DublinBikes.objects.values('position','stand_namr)
-
-#SELECT_NAME = DublinBikes.objects.values('stand_name')
+SELECT_ALL_DATA_VALUES = DublinBikes.objects.values('position', 'stand_number', 'stand_name', 'total_bike_stands',
+                                                    'available_bike_stands', 'available_bikes', 'last_update')
