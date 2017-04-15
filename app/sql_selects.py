8e3124f455d7031171a9c18f5a09319530246002
Eoin-Irwin <C13439802@mydit.ie>
2017-04-15 22:33:24 +0100

    Selects with all bike data

--- app/sql_selects.py
+++ app/sql_selects.py
@@ -1,4 +1,4 @@
 from app.models import DublinBikes
 
 SELECT_ALL_DATA_VALUES = DublinBikes.objects.values('position', 'stand_number', 'stand_name', 'total_bike_stands',
-                                                    'available_bike_stands', 'available_bikes', 'last_update')
+                                                    'available_bike_stands','available_bikes', 'last_update')
