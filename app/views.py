e7e9c67a68de02cda2a6b6d8ebdae1db2376eb58
Eoin-Irwin <C13439802@mydit.ie>
2017-04-19 21:08:32 +0100

    Update stations every 5 minutes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from app.dublin_bikes_api import json_data_import
+from app.models import DublinBikes
 from .sql_selects import SELECT_ALL_DATA_VALUES
 
 
@@ -13,6 +14,11 @@ def index(request):
     return TemplateResponse(request, 'app/index.html')
 
 
-def landing(request):
+def all_stations(request):
+    json_data = json_data_import()
+    for i in json_data:
+        DublinBikes.objects.filter(stand_number=i['number']).update(
+            available_bike_stands=i['available_bike_stands'], available_bikes=i['available_bikes'],
+            last_update=i['last_update'])
     all_data = SELECT_ALL_DATA_VALUES
     return TemplateResponse(request, 'app/all_bike_stations.html', {"data": all_data})
