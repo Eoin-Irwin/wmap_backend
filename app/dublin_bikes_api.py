61408024f8dfb02992ec5974d6b18f52c279bde1
Eoin-Irwin <C13439802@mydit.ie>
2017-04-19 21:09:00 +0100

    Datetime now update

--- app/dublin_bikes_api.py
+++ app/dublin_bikes_api.py
@@ -1,6 +1,9 @@
 # noinspection PyCompatibility
+import datetime
+
 import requests
 from django.contrib.gis.geos import Point
+from django.utils import timezone
 
 
 def json_data_import():
@@ -8,7 +11,7 @@ def json_data_import():
         'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a6c1b6c4bac2222a2b80e49abf91bbfddd45e5e2')
     dub_json_data = r.json()
     for i in dub_json_data:
-        i['last_update'] = 0
+        i['last_update'] = datetime.datetime.now()
         i['position'] = Point(float(i['position']['lng']), float(i['position']['lat']))
     return dub_json_data
 
