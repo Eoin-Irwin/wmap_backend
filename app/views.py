00c99e6586855e9afcedd22df2fde0e516cbfd51
Eoin-Irwin <C13439802@mydit.ie>
2017-04-20 20:41:39 +0100

    Added json data API

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+
+from app.distance_of_points import distance_checker
 from app.dublin_bikes_api import json_data_import
 from app.models import DublinBikes
 from .sql_selects import SELECT_ALL_DATA_VALUES
+
+
+def json_all_stations(request):
+    json_data = json_data_import()
+    for i in json_data:
+        DublinBikes.objects.filter(stand_number=i['number']).update(
+            available_bike_stands=i['available_bike_stands'], available_bikes=i['available_bikes'],
+            last_update=i['last_update'])
+    all_data = SELECT_ALL_DATA_VALUES
+    return HttpResponse(all_data)
 
 
 def view_json_data(request):
@@ -22,3 +34,8 @@ def all_stations(request):
             last_update=i['last_update'])
     all_data = SELECT_ALL_DATA_VALUES
     return TemplateResponse(request, 'app/all_bike_stations.html', {"data": all_data})
+
+
+def nearby(request):
+    check_dis = distance_checker()
+    return TemplateResponse(request, 'app/index.html')
